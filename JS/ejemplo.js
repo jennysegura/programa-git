430afb67f0230f5f4b5bbde93739189327a7bde4
jennysegura <tjenny023@gmail.com>
2023-12-06 18:49:40 -0500

    version 1

---
+++ JS/ejemplo.js
@@ -0,0 +1,16 @@
+var nombre = "freddy";
+var numero1 = 4;
+var frutas = ["naranjas", "peras", "manzanas"]
+var objetopersona = {
+    nombre: "freddy",
+    apellido: "camacho",
+    direccion: "Mz 23 casa 1"
+};
+
+
+function mensaje() {
+    alert(nombre)
+    alert(numero1)
+    alert(frutas[0])
+    alert("este objeto tiene el siguiente nombre:" + objetopersona.nombre + "y el apellido de este objeto es:" + objetopersona.apellido)
+}
